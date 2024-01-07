c99a7bead75a720a19195bf3476a62f7dadf75ca
rigojr <rigojunnior@gmail.com>
2024-01-07 18:32:04 -0500

    feat: add conversation status to model

--- backend/src/models/conversation.model.ts
+++ backend/src/models/conversation.model.ts
@@ -2,9 +2,18 @@ import { Document, model, Schema, SchemaTypes } from 'mongoose';
 import { UserDocument } from './user.model';
 import { MessageDocument } from './message.model';
 
+/**
+ * FIXME: DRY
+ */
+export enum ConversationStatus {
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected'
+}
 export interface ConversationDocument extends Document {
   'users': UserDocument[];
   'messages': MessageDocument[];
+  'status': ConversationStatus
 }
 
 const conversationSchema = new Schema<ConversationDocument>({
@@ -17,7 +26,11 @@ const conversationSchema = new Schema<ConversationDocument>({
     'type': SchemaTypes.ObjectId,
     'ref': 'Message',
     'required': true
-  }]
+  }],
+  'status': {
+    'type': String,
+    'enum': [ ConversationStatus.PENDING, ConversationStatus.ACCEPTED, ConversationStatus.REJECTED ]
+  }
 });
 
 export default model<ConversationDocument>('Conversation', conversationSchema);

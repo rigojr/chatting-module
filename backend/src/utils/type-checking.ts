2f63194c687dcedec7cd32305ddc593a6b70db64
rigojr <rigojunnior@gmail.com>
2024-01-07 20:08:28 -0500

    feat: add some type checking

--- backend/src/utils/type-checking.ts
+++ backend/src/utils/type-checking.ts
@@ -10,3 +10,17 @@
 export function isNullish(value: unknown): value is undefined | null {
   return value === undefined || value === null;
 }
+
+/**
+ * TODO:
+ */
+export function isEmpty<T>(value: ArrayLike<T>): boolean {
+  return value.length === 0
+}
+
+/**
+ * TODO:
+ */
+export function hasNullish<T>(collection: T[]): collection is Array<NonNullable<T>>{
+  return collection.some((value) => isNullish(value));
+}
